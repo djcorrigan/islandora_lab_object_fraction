53a1c82b4cb413d321d408c8d386d3a05387f577
Brent Mitton <brent.mitton@gmail.com>
2014-03-11 12:16:41 -0300

    added theme basics to fraction

---
+++ theme/fraction-theme.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * This is the template file for the specimen object page.
+ *
+ * Here we build an html page using the variables passed in by
+ * the islandora_bioinformatics_specimen_preprocess_islandora_specimen
+ * function.  Elements such as labels and buttons can be added here
+ */
+
+$path = drupal_get_path('module', 'islandora_bioinformatics_fraction');
+
+
+echo "<pre>";
+print_r($variables);
+echo "</pre>";
+?>
+
+
