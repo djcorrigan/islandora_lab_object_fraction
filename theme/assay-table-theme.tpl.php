ff54459d0abf98f962146fbdff777d59dfddf510
Brent Mitton <brent.mitton@gmail.com>
2014-04-14 16:28:10 -0300

    DEVELOP

--- theme/assay-table-theme.tpl.php
+++ theme/assay-table-theme.tpl.php
@@ -1,3 +1,31 @@
+<style>
+  .assay-hit{
+    background-color: red;
+  }
+
+  .assay-strong{
+    background-color: yellow;
+  }
+
+  .assay-medium{
+    background-color: orange;
+  }
+
+  .assay-low{
+    background-color: #808080;
+  }
+
+  .assay-none{
+    background-color: white;
+  }
+
+  .assay-inactive{
+    background-color: white;
+  }
+</style>
+<?php
+  print_r ($variables);
+?>
 <div>
   <table>
 
