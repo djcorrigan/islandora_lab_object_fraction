1a557d9f21482dae4839400c8a42bfdbd9c5cd5d
Brent Mitton <brent.mitton@gmail.com>
2014-04-14 16:16:24 -0300

    rapid fire changes

--- theme/assay-table-theme.tpl.php
+++ theme/assay-table-theme.tpl.php
@@ -1,11 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: brentmitton
- * Date: 2014-04-14
- * Time: 3:25 PM
- */
-
-$path = drupal_get_path('module', 'islandora_lab_object_assay');
-
-?>
+<h2> The shit ?</h2>
