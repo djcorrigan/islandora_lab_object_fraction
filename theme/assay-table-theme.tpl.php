3e3465c127fac642eb118fe5722b2bf4ad503f31
Brent Mitton <brent.mitton@gmail.com>
2014-04-14 18:41:58 -0300

    show me the var_dump

--- theme/assay-table-theme.tpl.php
+++ theme/assay-table-theme.tpl.php
@@ -19,6 +19,9 @@
   }
 
   function print_fraction_result($fraction, $assay){
+    echo "<pre>";
+    var_dump($fraction);
+    echo "</pre>";
     if(array_key_exists($fraction, $assay)){
       echo "HEYYYYYY" . $fraction[$assay]["name"];
     }else{
