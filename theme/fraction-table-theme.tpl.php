01eb72dfd91f8aa8fba4b1f95c21840ee51114f8
Brent Mitton <brent.mitton@gmail.com>
2014-04-15 01:12:11 -0300

    dumb on a whole new level

--- theme/fraction-table-theme.tpl.php
+++ theme/fraction-table-theme.tpl.php
@@ -62,11 +62,16 @@
 
   function print_fraction_result($fraction, $assay) {
 
-    if(array_key_exists($assay, $fraction["fraction"])){
-      $css_class = ($fraction["fraction"][$assay]["result"]) ? "assay-".$fraction["fraction"][$assay]["result"]:"assay-none";
-      echo "<td class='$css_class'></td>";
+    if (array_key_exists("fraction", $fraction)){
+      if (array_key_exists($assay, $fraction["fraction"])){
+        $css_class = ($fraction["fraction"][$assay]["result"]) ? "assay-".$fraction["fraction"][$assay]["result"]:"assay-none";
+        echo "<td class='$css_class'></td>";
+      }else{
+        echo "<td class='assay-none'>N</td>";
+      }
     }else{
-      echo "<td class='assay-none'></td>";
+      echo "<td class='assay-none'>N</td>";
+
     }
 
   }
